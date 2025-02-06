48ee79aef0138bbfa3d935d303d3bf1daa246e8b
Li Feng <lifeng.phys@gmail.com>
2025-02-06 13:48:54 -0500

    asdfgh

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from H_shape import Solver, save
 
-r = [-10] + [-200] * 5
+r = [-2] + [-100] * 5
 solver = Solver(r)
 solver.find_all()
 solution = solver.get_det()
@@ -8,11 +8,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = list(solution.keys())
+x = x[::-1]
 y = list(solution.values())
 
 plt.plot(x, y)
 plt.xlabel('Sample size')
 plt.ylabel('Determinant')
 plt.title('Determinants vs Sample size')
-plt.savefig("determinant_ss_200.pdf") 
+plt.savefig("det_vs_ss_200.pdf") 
+plt.close()
+
+plt.scatter(x, y, s=1)
+plt.xlabel('Sample size')
+plt.ylabel('Determinant')
+plt.title('Determinants vs Sample size')
+plt.savefig("scatter_200.pdf") 
 plt.close()
