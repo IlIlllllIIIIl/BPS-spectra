a1acc0c1d584d061006a1dd2f83c8ba1c7ce9809
Li Feng <lifeng.phys@gmail.com>
2025-02-05 20:34:50 -0500

    more!!

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 from H_shape import Solver, save
 
-r = [-10] + [-500] * 5 
+r = [-10] + [-200] * 5 
 solver = Solver(r)
-solver.select(2)
-solver.select(3)
-solver.select(4)
-solution = solver.get_solutions()
-for i in solution.keys():
-    path_names = f"data/det_{i}_500.csv"
-    save(solution[i], path_names)
+for i in range(1, 31):
+    solver.select(i)
+    path_names = f"data/test_det_{i}_500.csv"
+    save(solver.get_solutions()[i], path_names)
