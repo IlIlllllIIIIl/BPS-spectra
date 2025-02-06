d63fdfdd1cb28ea4c0e576ab7e4a18eab1e9899c
Li Feng <lifeng.phys@gmail.com>
2025-02-05 19:02:15 -0500

    rename

--- test.py
+++ test.py
@@ -7,5 +7,5 @@ solver.select(3)
 solver.select(4)
 solution = solver.get_solutions()
 for i in solution.keys():
-    path_names = f"det_{i}_500.csv"
+    path_names = f"data/det_{i}_500.csv"
     save(solution[i], path_names)
