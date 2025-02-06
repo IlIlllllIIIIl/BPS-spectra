9c28abd9b3df8fcb2bdad8fb374c969fe6bcdde3
Li Feng <lifeng.phys@gmail.com>
2025-02-05 22:20:09 -0500

    update to det = 31
    before i go to bed

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from H_shape import Solver, save
 
-r = [-10] + [-200] * 5 
+r = [-10] + [-300] * 5 
 solver = Solver(r)
-for i in range(1, 31):
+for i in range(1, 101):
     solver.select(i)
-    path_names = f"data/test_det_{i}_500.csv"
+    path_names = f"data/test_det_{i}_300.csv"
     save(solver.get_solutions()[i], path_names)
